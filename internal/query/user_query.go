c70c4909bf11b4f0a64b7c85a5d3a25c2405c12a
A Ruiz <luis.alonso.16@hotmail.com>
2020-08-12 01:51:09 -0600

    Add user query for CQRS

---
+++ internal/query/user_query.go
@@ -0,0 +1,41 @@
+package query
+
+import (
+	"context"
+	"github.com/alexandria-oss/identity-api/internal/common"
+	"github.com/alexandria-oss/identity-api/internal/domain"
+)
+
+type UserQueryImp struct {
+	repository domain.UserQueryRepository
+}
+
+func NewUserQueryImp(r domain.UserQueryRepository) *UserQueryImp {
+	return &UserQueryImp{
+		repository: r,
+	}
+}
+
+func (q *UserQueryImp) Get(ctx context.Context, id string) (*domain.User, error) {
+	ctxI, _ := context.WithCancel(ctx)
+	return q.repository.FetchOne(ctxI, id)
+}
+
+func (q *UserQueryImp) List(ctx context.Context, token string, size int, filterMap common.FilterMap) (users []*domain.User,
+	nextToken common.QueryToken, err error) {
+	// Request next token
+	nextSize := size + 1
+
+	ctxI, _ := context.WithCancel(ctx)
+	users, err = q.repository.Fetch(ctxI, token, nextSize, filterMap)
+	if err != nil {
+		return
+	}
+
+	if size >= len(users) {
+		nextToken = common.QueryToken(users[len(users)-1].Sub)
+		users = users[:len(users)-1]
+	}
+
+	return
+}
